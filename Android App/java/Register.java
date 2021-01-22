3e888d11feeff10678de91c14ebd938d3d4ba3fb
Avery Curnel <61637647+ACurnel@users.noreply.github.com>
2021-01-21 21:20:05 -0600

    Add files via upload

---
+++ Android App/java/Register.java
@@ -0,0 +1,92 @@
+package com.example.luggagecarrier;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Register extends AppCompatActivity {
+
+    /* Define the UI elements */
+    private EditText userName, userPassword, userEmail;
+    private Button regButton;
+    private TextView userLogin;
+    private FirebaseAuth firebaseAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        setupUIViews();
+
+        //initialize database
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        regButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(validate()){
+                    //Upload to the database
+                    String user_email = userEmail.getText().toString().trim();
+                    String user_password = userPassword.getText().toString().trim();
+
+                    firebaseAuth.createUserWithEmailAndPassword(user_email, user_password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()) {
+                                Toast.makeText(Register.this, "Registration Successful", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(Register.this, MainActivity.class));
+                            }else{
+                                Toast.makeText(Register.this, "Registration Failed", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+        userLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Register.this, MainActivity.class));
+            }
+        });
+    }
+
+    /* function to assign elements to correct fields */
+    private void setupUIViews() {
+        userName = (EditText)findViewById(R.id.etUserName);
+        userEmail = (EditText)findViewById(R.id.etUserEmail);
+        userPassword = (EditText)findViewById(R.id.etUserPassword);
+        regButton = (Button)findViewById(R.id.btnRegistration);
+        userLogin = (TextView)findViewById(R.id.tvUserLogin);
+    }
+
+    private Boolean validate(){
+        Boolean result = false;
+        String name = userName.getText().toString();
+        String password = userPassword.getText().toString();
+        String email = userEmail.getText().toString();
+
+        if(name.isEmpty() || password.isEmpty() || email.isEmpty()){
+            Toast.makeText(this, "Please enter all the details", Toast.LENGTH_SHORT).show();
+        }else {
+            result = true;
+        }
+
+        return result;
+    }
+
+}
