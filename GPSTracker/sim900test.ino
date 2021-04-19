4fb547d01ac99163aea16842f5d742ab7da209f1
kevinkim7985 <32913005+kevinkim7985@users.noreply.github.com>
2021-04-19 11:18:16 -0500

    Add files via upload

---
+++ GPSTracker/sim900test.ino
@@ -0,0 +1,49 @@
+
+#include <SoftwareSerial.h>
+
+
+
+//Create software serial object to communicate with SIM900
+SoftwareSerial mySerial(2,3); //SIM900 Tx & Rx is connected to Arduino #7 & #8
+
+
+void setup()
+{
+  //Begin serial communication with Arduino and Arduino IDE (Serial Monitor)
+  Serial.begin(9600);
+  
+  //Begin serial communication with Arduino and SIM900
+  mySerial.begin(9600);
+
+  Serial.println("Initializing...");
+  
+
+  mySerial.println("AT"); //Handshaking with SIM900
+  updateSerial();
+  mySerial.println("AT+CSQ"); //Signal quality test, value range is 0-31 , 31 is the best
+  updateSerial();
+  mySerial.println("AT+CCID"); //Read SIM information to confirm whether the SIM is plugged
+  updateSerial();
+  mySerial.println("AT+CREG?"); //Check whether it has registered in the network
+  updateSerial();
+  delay(5000);
+  //mySerial.println(""AT+SAPBR=3,1,\"Contype\",\"GPRS\");
+}
+
+void loop() 
+{
+  updateSerial();
+}
+
+void updateSerial()
+{
+  delay(500);
+  while (Serial.available()) 
+  {
+    mySerial.write(Serial.read());//Forward what Serial received to Software Serial Port
+  }
+  while(mySerial.available()) 
+  {
+    Serial.write(mySerial.read());//Forward what Software Serial received to Serial Port
+  }
+}
